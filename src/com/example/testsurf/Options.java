8cf186ada7f54081a0052ace067dac819da28316
Sean <Sean@128.138.19.80>
2011-04-16 13:05:11 -0600

    Fixed warnings in Marina's code and api target

--- src/com/example/testsurf/Options.java
+++ src/com/example/testsurf/Options.java
@@ -1,15 +1,10 @@
 package com.example.testsurf;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.RadioButton;
 
 public class Options extends Menu {
 
