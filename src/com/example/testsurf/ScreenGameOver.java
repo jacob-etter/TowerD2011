8bdfb8d1f644715402fa0b10723a68654d023480
Sean <Sean@Laptop-PC>
2011-04-23 16:20:21 -0600

    added gameover screen

---
+++ src/com/example/testsurf/ScreenGameOver.java
@@ -0,0 +1,40 @@
+package com.example.testsurf;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+/**
+ * Displays the startup menu
+ * @author Sean
+ *
+ */
+public class ScreenGameOver extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        //force landscape
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gameover);
+
+    Button mainmenu = (Button) findViewById(R.id.ButtonMainMenu);
+    mainmenu.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+            Intent myIntent = new Intent(view.getContext(), Menu.class);
+            myIntent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
+            startActivity(myIntent);
+        }
+    });
+    }
+}
